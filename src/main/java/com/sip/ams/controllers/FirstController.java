823262fa74580ca48e451665687224f6ca7457db
Maël Mainsard <mainsardm@gmail.com>
2024-05-02 12:32:14 +0200

    First Push

---
+++ src/main/java/com/sip/ams/controllers/FirstController.java
@@ -0,0 +1,22 @@
+package com.sip.ams.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FirstController {
+	
+	@GetMapping("/info")
+	//@ResponseBody
+	public String info() {
+		return "first/info";
+	}
+	
+	@GetMapping("/details")
+	//@ResponseBody
+	public String detail() {
+		return "first/detail";
+	}
+
+
+}
